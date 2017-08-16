c0d543b0443ab39ddb9cb0f33055fd17d6a223b4
Manuel  Cervantes <manucervantes@MacBook-Pro-de-Manuel.local>
2017-08-15 22:52:14 -0500

    create user

---
+++ apigoods/index.php
@@ -0,0 +1,15 @@
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src = "https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>APIGOODS</title>
+  </head>
+  <body>
+
+  </body>
+</html>
