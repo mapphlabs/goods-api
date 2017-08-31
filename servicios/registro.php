6b3581431e712e946a47dfef08118ea2563508aa
agustin razo <arazo@virtualmarket.mx>
2017-08-30 21:20:12 -0500

    servicios

--- servicios/registro.php
+++ servicios/registro.php
@@ -4,19 +4,19 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "HOLA MUNDO3";
+echo "HOLA MUNDO4";
 
 require_once("db.php");
 
 if(!isset($_GET['usuario'])){
 	echo json_encode(error("Usuario requerido",null));
-}/*else if(!isset($_GET['contrasenia'])){
+}else if(!isset($_GET['contrasenia'])){
 	echo json_encode(error("Contraseña requerida",null));
 }else if(!isset($_GET['contrasenia2'])){
 	echo json_encode(error("Todos los campos son requeridos",null));
 }else if($_GET['contrasenia']!=$_GET['contrasenia2'])){
 	echo json_encode(error("Las contraseñas no coinciden",null));
-}/*else{
+}else{
 	$db = new Db();
 	$query="INSERT INTO usuario (usuario,contrasenia,id_perfil_usuario) VALUES ('".$_GET['usuario']."','".$_GET['contrasenia']."',0)";
 	$result=$db->query($query);
@@ -26,7 +26,7 @@ if(!isset($_GET['usuario'])){
 	}else{
 		echo json_encode(error("Error en base de datos",null));
 	}
-}*/
+}
 
 function error($msg,$data){
 	$obj=array(
