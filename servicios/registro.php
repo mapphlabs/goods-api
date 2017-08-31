39f75c1397b3f9cb23ebb40dfd46316c6f81eb6b
agustin razo <arazo@virtualmarket.mx>
2017-08-30 21:26:58 -0500

    servicios

--- servicios/registro.php
+++ servicios/registro.php
@@ -15,7 +15,7 @@ if(!isset($_GET['usuario'])){
 }else if($_GET['contrasenia']!=$_GET['contrasenia2']){
 	echo json_encode(error("Las contraseñas no coinciden",null));
 }else{
-	/*$db = new Db();
+	$db = new Db();
 	$query="INSERT INTO usuario (usuario,contrasenia,id_perfil_usuario) VALUES ('".$_GET['usuario']."','".$_GET['contrasenia']."',0)";
 	$result=$db->query($query);
 
@@ -23,7 +23,7 @@ if(!isset($_GET['usuario'])){
 		echo json_encode(ok("Usuario creado satisfactoriamente",$result));
 	}else{
 		echo json_encode(error("Error en base de datos",null));
-	}*/
+	}
 }
 
 function error($msg,$data){
